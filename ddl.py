1a3d433d7f96fbed3ac98d642a0e5a2a2f2127f2
Jônatas da Silva <jonatas.silva.roots@gmail.com>
2018-07-03 16:13:34 -0300

    Ajuste
    ajustes

--- ddl.py
+++ ddl.py
@@ -22,6 +22,8 @@ print("'database' selecionado ...")
 collisions = database.collisions
 print("'collection' selecionada ...")
 
+cabecalho = []
+conteudo = []
 '''  -------------- Estrutura e funções de pré-processamento. ---------------- '''
 # Variaveis locais para os documentos JSON
 data = {}
@@ -34,64 +36,64 @@ motorist = {}
 pedestrians = {}
 
 #Funções para construção dos documentos JSON
-def createData (cabecalho, dado):
-    data[cabecalho] = dado
+def createData (cabe, dado):
+    data[cabe] = dado
 
-def createLocation (cabecalho, dado):
-    local[cabecalho] = dado
+def createLocation (cabe, dado):
+    local[cabe] = dado
 
-def createPerson (cabecalho, dado):
-    person[cabecalho] = dado
+def createPerson (cabe, dado):
+    person[cabe] = dado
 
-def createCyclist (cabecalho, dado):
-    cyclist[cabecalho] = dado
+def createCyclist (cabe, dado):
+    cyclist[cabe] = dado
 
-def createPedestrians (cabecalho, dado):
-    pedestrians[cabecalho] = dado
+def createPedestrians (cabe, dado):
+    pedestrians[cabe] = dado
 
-def createMotorist (cabecalho, dado):
-    motorist[cabecalho] = dado
+def createMotorist (cabe, dado):
+    motorist[cabe] = dado
 
-def createVehicle (cabecalho, dado):
-    vehicle[cabecalho] = dado
+def createVehicle (cabe, dado):
+    vehicle[cabe] = dado
 
-def createFactor (cabecalho, dado):
-    factor[cabecalho] = dado
+def createFactor (cabe, dado):
+    factor[cabe] = dado
 
 # Insere o conteúdo do arquivo nos locais devidos
-def createDocument (cabecalho, dado):
-    createCyclist(cabecalho[14], dado[14])
-    createCyclist(cabecalho[15], dado[15])
-    createMotorist(cabecalho[16], dado[16])
-    createMotorist(cabecalho[17], dado[17])
-    createPedestrians(cabecalho[12], dado[12])
-    createPedestrians(cabecalho[13], dado[13])
-    createPerson(cabecalho[10], dado[10])
-    createPerson(cabecalho[11], dado[11])
+def createDocument ():
+    createCyclist(cabecalho[14], conteudo[14])
+    createCyclist(cabecalho[15], conteudo[15])
+    createMotorist(cabecalho[16], conteudo[16])
+    createMotorist(cabecalho[17], conteudo[17])
+    createPedestrians(cabecalho[12], conteudo[12])
+    createPedestrians(cabecalho[13], conteudo[13])
+    createPerson(cabecalho[10], conteudo[10])
+    createPerson(cabecalho[11], conteudo[11])
     createPerson("CYCLISTS", cyclist)
     createPerson("MOTORISTS", motorist)
     createPerson("PEDESTRIANS", pedestrians)
-    createFactor(cabecalho[18], dado[18])
-    createFactor(cabecalho[19], dado[19])
-    createFactor(cabecalho[20], dado[20])
-    createFactor(cabecalho[21], dado[21])
-    createFactor(cabecalho[22], dado[22])
-    createVehicle(cabecalho[24], dado[24])
-    createVehicle(cabecalho[25], dado[25])
-    createVehicle(cabecalho[26], dado[26])
-    createVehicle(cabecalho[27], dado[27])
-    createVehicle(cabecalho[28], dado[28])
-    createLocation(cabecalho[2], dado[2])
-    createLocation(cabecalho[3], dado[3])
-    createLocation(cabecalho[4], dado[4])
-    createLocation(cabecalho[5], dado[5])
-    createLocation(cabecalho[6], dado[6])
-    createLocation(cabecalho[7], dado[7])
-    createLocation(cabecalho[8], dado[8])
-    createLocation(cabecalho[9], dado[9])
-    createData("_id", dado[23])
-    createData(cabecalho[0], dado[0])
-    createData(cabecalho[1], dado[1])
+    createFactor(cabecalho[18], conteudo[18])
+    createFactor(cabecalho[19], conteudo[19])
+    createFactor(cabecalho[20], conteudo[20])
+    createFactor(cabecalho[21], conteudo[21])
+    createFactor(cabecalho[22], conteudo[22])
+    createVehicle(cabecalho[24], conteudo[24])
+    createVehicle(cabecalho[25], conteudo[25])
+    createVehicle(cabecalho[26], conteudo[26])
+    createVehicle(cabecalho[27], conteudo[27])
+    createVehicle(cabecalho[28], conteudo[28])
+    createLocation(cabecalho[2], conteudo[2])
+    createLocation(cabecalho[3], conteudo[3])
+    createLocation(cabecalho[4], conteudo[4])
+    createLocation(cabecalho[5], conteudo[5])
+    createLocation(cabecalho[6], conteudo[6])
+    createLocation(cabecalho[7], conteudo[7])
+    createLocation(cabecalho[8], conteudo[8])
+    createLocation(cabecalho[9], conteudo[9])
+    createData("_id", conteudo[23])
+    createData(cabecalho[0], conteudo[0])
+    createData(cabecalho[1], conteudo[1])
     createData("LOCAL", local)
     createData("PERSONS", person)
     createData("FACTORS", factor)
@@ -106,7 +108,7 @@ def writeDocument (cabecalho):
             file.write(campo)
             file.write('\n')
         file.close()
-
+'''>>>>>>>>>> Função pré-processamento dos dados <<<<<<<<<<'''
 def readCSV (cabecalho, conteudo):
     with open('base.csv', 'r', encoding='utf8') as file:
         dados = csv.reader(file, delimiter=',')
@@ -115,28 +117,31 @@ def readCSV (cabecalho, conteudo):
         for linhas in dados:
             for j, row in enumerate(linhas):
                 if flag:
-                    break
                     conteudo.insert(j, row)
                     if j == 28:
-                        createDocument(cabecalho, conteudo)
-                        #writeDocument()
+                        createDocument()
                         insertMongo()
                         conteudo.clear()
                 else:
                     cabecalho.insert(j, row)
                     if row == "VEHICLE TYPE CODE 5":
-                        writeDocument(cabecalho)
                         flag = True
                 # Após ler todos os dados grava no arquivo e insere no banco.      
             if flag:
                 break
         file.close()
-'''  -------------- Acaba aqui as funções de pré-processamento. ---------------- '''
+'''-------------- Acaba aqui as funções de pré-processamento. ----------------'''
+def dropDatabase ():
+    print("Coleção: x será excluido")
+    x = cillisions.drop()
+
+#create collection and create database, ao criar a coleção o base de dados se não existir será criada;
+def createCollection ():
+    #http://api.mongodb.com/python/current/api/pymongo/collection.html
 
 if __name__ == "__main__":
     #parameters = input("Insira a ação DDL: ")
-    cabecalho = []
-    conteudo = []
+
     print("O arquivo 'CSV' estar sendo carregado, por favor agarde alguns minutos...")
     print("AUARDE ALGUNS MINUTOS...")
     readCSV(cabecalho, conteudo)
